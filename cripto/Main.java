4ac1c2bf6fbb6a260ff02d950a3c43becbc4c5ec
Pascual Barrer <pbarrer@cifpfbmoll.eu>
2021-11-02 23:11:24 +0100

    Simplificado centrar en pantalla

--- cripto/Main.java
+++ cripto/Main.java
@@ -1,7 +1,6 @@
 package cripto;
 
 import javax.swing.*;
-import java.awt.*;
 
 class Main extends JFrame {
 
@@ -16,21 +15,9 @@ class Main extends JFrame {
 
         this.pack();
 
-        centrarEnPantalla();
+        this.setLocationRelativeTo(null);
 
         this.setVisible(true);
-    }
-
-    private void centrarEnPantalla() {
-        // Averigua el tamaño de la pantalla
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int anchuraPantalla = (int) screenSize.getWidth();
-        int alturaPantalla = (int) screenSize.getHeight();
-
-        // Centra la ventana sabiendo su tamaño
-        int anchuraVentana = this.getWidth();
-        int alturaVentana = this.getHeight();
-        this.setLocation((anchuraPantalla - anchuraVentana) / 2, (alturaPantalla - alturaVentana) / 2);
     }
 
     public static void main(String[] args) {
